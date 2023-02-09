7892111e575f90f017bdc506b990040ece701f04
Odili Chinedum Christian <odilichinedum@gmail.com>
2023-02-10 00:21:40 +0100

    Added migration, sql database and updated other pages

--- hello.py
+++ hello.py
@@ -1,21 +1,31 @@
 from datetime import datetime
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 # creating flask instance
 app = Flask(__name__)
 
-# add database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+# adding a sqlite database
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+# 
+# adding a mysql and pymysql database
+# 
+# my main password is 'Blackbro1641@' but i had to escape the @ to become %40. This will interprete @ as %40 in the password since special characters are not directly allowed.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Blackbro1641%40@localhost/user_db'
 
 # Creating secret key
 app.config['SECRET_KEY'] = 'my super secret key that no one is required to know'
 
 # Initialize the database
 db = SQLAlchemy(app)
+
+# Initializing the migrate extension
+migrate = Migrate(app, db)
 
 
 # Creating model class for database mapping
@@ -25,7 +35,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(200), nullable=False, unique=True)
-    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    favourite_color = db.Column(db.String(200))
+    date_added = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     
     # Creating function strings
     def __repr__(self):
@@ -78,6 +89,7 @@ class NamerForm(FlaskForm):
 class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
+    favourite_color = StringField("Favourite Color")
     submit = SubmitField("Submit")
 
 
@@ -123,7 +135,7 @@ def name():
         flash('Form Submitted Successfully', 'success')
     return render_template('name.html', name=name, form=form)
 
-
+# add new users
 @app.route('/user/add', methods=['GET', 'POST'])
 def add_user():
     name = None
@@ -134,14 +146,16 @@ def add_user():
         if user is None:
             user = User(
                 name=form.name.data,
-                email=form.email.data
+                email=form.email.data,
+                favourite_color=form.favourite_color.data
             )
             db.session.add(user)
             db.session.commit()
             name = form.name.data
             form.name.data = ''
             form.email.data = ''
-            flash(f'User with name {name} was Added Successfully', 'success') 
+            form.favourite_color.data = ''
+            flash(f'User with name {name.capitalize()} was Added Successfully', 'success') 
         else:
             email = form.email.data
             flash(f'User with this email {email} already exist. Please use another email', 'danger')
@@ -151,8 +165,65 @@ def add_user():
     
     return render_template('add_user.html', form=form, all_users=all_users)
     
+# update specific user database record
+@app.route('/user/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    form = UserForm()
     
+    name_to_update = User.query.get_or_404(id)
+    if request.method == 'POST':
+        name_to_update.name = request.form['name']
+        name_to_update.email = request.form['email']
+        name_to_update.favourite_color = request.form['favourite_color']
+        
+        try:
+            db.session.commit()
+            flash('User details updated successfully', 'success')
+            
+            # email = name_to_update.email
+            
+            return render_template('update.html', form=form, name_to_update=name_to_update)
+        except:
+            flash('Error updating this user. Please try again', 'danger')
+            return render_template('update.html', form=form, name_to_update=name_to_update)
+        
+    else:
+        return render_template('update.html', form=form, name_to_update=name_to_update)
+        
+
+        
+   # delete specific user database record
+@app.route('/delete/<int:id>', methods=['GET', 'POST'])
+def delete(id):
+    # form = UserForm()
     
+    user_to_delete = User.query.get_or_404(id)
+    # if request.method == 'POST':
+    #     name_to_update.name = request.form['name']
+    #     name_to_update.email = request.form['email']
+    #     name_to_update.favourite_color = request.form['favourite_color']
+        
+    try:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        flash('User deleted successfully', 'success')
+        
+         # Display all users in the database and order by date added
+        all_users = User.query.order_by(User.date_added)
+        return redirect(url_for('add_user'))
+        
+        # return render_template('add_user.html', all_users=all_users, form=form)
+        
+        # return render_template('update.html', form=form, name_to_update=name_to_update)
+    except:
+        flash('Error deleting this user. Please try again', 'danger')
+        # return render_template('add_user.html', all_users=all_users, form=form)
+    return render_template('add_user.html', all_users=all_users)
+        
+        
+
+        
+   
 # creating custom error pages
 
 
@@ -167,3 +238,6 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
     
+    
+if __name__ == '__main__':
+    app.run(debug = True)
