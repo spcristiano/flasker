5ee46de64008bf1c047909c18f3a2d1dae328ec4
Odili Chinedum Christian <odilichinedum@gmail.com>
2023-02-09 09:45:24 +0100

    added navbar, base.html,user.html,404.html,500.html,index.html,

--- hello.py
+++ hello.py
@@ -1,8 +1,58 @@
 from flask import Flask, render_template
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # creating flask instance
 app = Flask(__name__)
+
+# Creating secret key
+app.config['SECRET_KEY'] = 'my super secret key that no one is required to know'
+
+
+# creating a form class
+
+#These are some flask wtf fields
+# BooleanField
+# DateField
+# DateTimeField
+# DecimalField
+# FileField
+# HiddenField
+# MultipleField
+# FieldList
+# FloatField
+# FormField
+# IntegerField
+# PasswordField
+# RadioField
+# SelectField
+# SelectMultipleField
+# SubmitField
+# StringField
+# TextAreaField
+
+# These are some flask wft field validators
+# DataRequired
+# Email
+# EqualTo
+# InputRequired
+# IPAddress
+# Length
+# MacAddress
+# NumberRange
+# Optional
+# Regexp
+# URL
+# UUID
+# AnyOf
+# NoneOf
+
+class NamerForm(FlaskForm):
+    name = StringField("What is your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+    
+
 
 # '''
 # # These are some jinger templating functions
@@ -16,6 +66,7 @@ app = Flask(__name__)
 
 # '''
 # creating a route decorator
+
 @app.route('/')
 def index():
     first_name = 'John'
@@ -24,7 +75,26 @@ def index():
     return render_template('index.html', first_name=first_name, stuff=stuff,favourite_pizzars=favourite_pizzars)
 
 
+@app.route('/user/<username>')
+def profile(username):
+    username = 'John'
+    first_name = 'Mike'
+    stuff =  'This is a <b>Bold Text</b>'
+    favourite_pizzars = ['pepperoni','Marka','melein',43, 'mandae',41]
+    return render_template('user.html', first_name=first_name, stuff=stuff,favourite_pizzars=favourite_pizzars)
 
+
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('name.html', name=name, form=form)
+    
 # creating custom error pages
 
 # invalid url
